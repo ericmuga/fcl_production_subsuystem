2cb8eac03eab08e5fb662d57d58bc723fb7f4e9b
Ephantus karanja <ephantusslogan@gmail.com>
2022-05-04 10:51:16 +0300

    added modal title

--- resources/views/butchery/products_list_deboning.blade.php
+++ resources/views/butchery/products_list_deboning.blade.php
@@ -7,7 +7,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title">Add Product Processes</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
