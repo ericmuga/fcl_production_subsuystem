4f32f4270289c9527d929d87d72e54574828ed5e
Ephantus karanja <ephantusslogan@gmail.com>
2022-10-26 16:11:20 +0300

    add blocked items list

--- app/Http/Controllers/SausageController.php
+++ app/Http/Controllers/SausageController.php
@@ -182,8 +182,9 @@ class SausageController extends Controller
 
         $filter = '';
 
-        $items = Cache::remember('items_list', now()->addHours(10), function () {
+        $items = Cache::remember('items_list_sausage', now()->addHours(10), function () {
             return DB::table('items')
+                ->where('blocked', '!=', 1)
                 ->select('code', 'barcode', 'description', 'qty_per_unit_of_measure', 'unit_count_per_crate')
                 ->get();
         });
