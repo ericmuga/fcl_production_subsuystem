3fc8df9918c4452620bf877d64dfd7be33f8cea3
Ephantus Karanja <ephantusslogan@gmail.com>
2023-06-07 09:52:16 +0300

    make closed batch editable

--- resources/views/chopping/production-lines.blade.php
+++ resources/views/chopping/production-lines.blade.php
@@ -15,7 +15,7 @@
                     </div>
                     <div class="col-md-5">
 
-                        @if ($lines->first()->status == 'open')
+                        @if ($lines->first()->status == 'open' || $lines->first()->status == 'closed')
                         <button type="button" onClick="editModal()" class="btn btn-primary" id="#editModalShow"><i
                                 class="fas fa-pencil-alt"></i>
                             Edit Batch
