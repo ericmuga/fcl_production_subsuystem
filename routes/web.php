94b86ea52845945afce38588af00af2a3124b0c5
Ephantus Karanja <ephantusslogan@gmail.com>
2023-10-03 11:06:16 +0300

    name route update

--- routes/web.php
+++ routes/web.php
@@ -224,6 +224,6 @@ Route::prefix('asset')->group(function () {
     Route::get('/create', [AssetController::class, 'createMovement'])->name('create_movement');
     Route::post('/save', [AssetController::class, 'saveMovement'])->name('save_movement');
     Route::get('/fetch-data', [AssetController::class, 'fetchData'])->name('assets_fetch_data');
-    Route::post('/check-user', [AssetController::class, 'validateUserAssets'])->name('validateUser');
+    Route::post('/check-user', [AssetController::class, 'validateUserAssets'])->name('validateUserAsset');
 });
 /*-------------End Assets------------------ */
