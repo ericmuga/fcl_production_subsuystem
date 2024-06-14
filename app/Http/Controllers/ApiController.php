4e98a98e99837c4b78a4ecd4bbb1bb961463b1ee
Ephantus Karanja <ephantusslogan@gmail.com>
2024-06-14 14:58:58 +0300

    refactored the controller, added validations

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,23 +5,46 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
 class ApiController extends Controller
 {
-    private function parseDates(Request $request)
+    private function parseDates($from_date, $to_date)
     {
         return [
-            'from_date' => Carbon::parse($request->from_date),
-            'to_date' => Carbon::parse($request->to_date),
+            'from_date' => Carbon::parse($from_date),
+            'to_date' => Carbon::parse($to_date),
         ];
+    }
+
+    private function fetchData($table, $columns, $from_date, $to_date)
+    {
+        return DB::table($table . ' as a')
+            ->whereDate('a.created_at', '>=', $from_date)
+            ->whereDate('a.created_at', '<=', $to_date)
+            ->select($columns)
+            ->orderByDesc('a.created_at')
+            ->get();
+    }
+
+    private function validateDateRange(Request $request)
+    {
+        return validator()->make($request->all(), [
+            'from_date' => 'required|date|before_or_equal:to_date',
+            'to_date' => 'required|date|after_or_equal:from_date',
+        ]);
     }
 
     public function getSlaughterData(Request $request)
     {
-        $dates = $this->parseDates($request);
+        $validator = $this->validateDateRange($request);
 
-        $from_date = $dates['from_date'];
-        $to_date = $dates['to_date'];
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $dates = $this->parseDates($request->from_date, $request->to_date);
 
         $columns = [
             'receipt_no', 'slapmark', 'item_code', 'vendor_no', 'vendor_name', 
@@ -29,96 +52,99 @@ class ApiController extends Controller
             'classification_code', 'created_at'
         ];
 
-        $slaughter_data = DB::table('slaughter_data as a')
-            ->whereDate('a.created_at', '>=', $from_date)
-            ->whereDate('a.created_at', '<=', $to_date)
-            ->select($columns)
-            ->orderByDesc('created_at')
-            ->get();
+        $slaughter_data = $this->fetchData('slaughter_data', $columns, $dates['from_date'], $dates['to_date']);
 
         return response()->json($slaughter_data);
     }
 
     public function missingSlapData(Request $request)
     {
-        $dates = $this->parseDates($request);
+        $validator = $this->validateDateRange($request);
 
-        $from_date = $dates['from_date'];
-        $to_date = $dates['to_date'];
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $dates = $this->parseDates($request->from_date, $request->to_date);
 
         $columns = [
-            'a.slapmark', 'a.item_code', 'a.actual_weight', 'a.net_weight', 'a.settlement_weight','a.meat_percent','a.classification_code', 'a.created_at'
+            'a.slapmark', 'a.item_code', 'a.actual_weight', 'a.net_weight', 'a.settlement_weight',
+            'a.meat_percent', 'a.classification_code', 'a.created_at'
         ];
 
-        $slaps = DB::table('missing_slap_data as a')
-            ->whereDate('a.created_at', '>=', $from_date)
-            ->whereDate('a.created_at', '<=', $to_date)
-            ->select($columns)
-            ->orderByDesc('a.created_at')
-            ->get();
+        $slaps_data = $this->fetchData('missing_slap_data', $columns, $dates['from_date'], $dates['to_date']);
 
-        return response()->json($slaps);
+        return response()->json($slaps_data);
     }
 
     public function getBeheadingData(Request $request)
     {
-        $dates = $this->parseDates($request);
+        $validator = $this->validateDateRange($request);
 
-        $from_date = $dates['from_date'];
-        $to_date = $dates['to_date'];
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $dates = $this->parseDates($request->from_date, $request->to_date);
 
         $columns = [
-            'item_code','no_of_carcass','actual_weight','net_weight','process_code','return_entry', 'a.created_at'
+            'item_code', 'no_of_carcass', 'actual_weight', 'net_weight', 'process_code',
+            'return_entry', 'a.created_at'
         ];
 
-        $beheading_data = DB::table('beheading_data as a')
-            ->whereDate('a.created_at', '>=', $from_date)
-            ->whereDate('a.created_at', '<=', $to_date)
-            ->select($columns)
-            ->orderByDesc('created_at')
-            ->get();
+        $beheading_data = $this->fetchData('beheading_data', $columns, $dates['from_date'], $dates['to_date']);
 
         return response()->json($beheading_data);
     }
 
     public function getBrakingData(Request $request)
     {
-        $dates = $this->parseDates($request);
+        $validator = $this->validateDateRange($request);
 
-        $from_date = $dates['from_date'];
-        $to_date = $dates['to_date'];
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $dates = $this->parseDates($request->from_date, $request->to_date);
 
         $columns = [
-            'carcass_type','item_code','actual_weight','net_weight','process_code','product_type','no_of_items', 'created_at'
+            'carcass_type', 'item_code', 'actual_weight', 'net_weight', 'process_code',
+            'product_type', 'no_of_items', 'created_at'
         ];
 
-        $butchery_data = DB::table('butchery_data as a')
-            ->whereDate('a.created_at', '>=', $from_date)
-            ->whereDate('a.created_at', '<=', $to_date)
-            ->select($columns)
-            ->orderByDesc('created_at')
-            ->get();
+        $braking_data = $this->fetchData('butchery_data', $columns, $dates['from_date'], $dates['to_date']);
 
-        return response()->json($butchery_data);
+        return response()->json($braking_data);
     }
 
     public function getDeboningData(Request $request)
     {
-        $dates = $this->parseDates($request);
+        $validator = $this->validateDateRange($request);
 
-        $from_date = $dates['from_date'];
-        $to_date = $dates['to_date'];
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $dates = $this->parseDates($request->from_date, $request->to_date);
 
         $columns = [
-            'item_code','actual_weight','net_weight','process_code','product_type','no_of_pieces','no_of_crates','splitted','narration', 'created_at'
+            'item_code', 'actual_weight', 'net_weight', 'process_code', 'product_type',
+            'no_of_pieces', 'no_of_crates', 'splitted', 'narration', 'created_at'
         ];
 
-        $deboning_data = DB::table('deboned_data as a')
-            ->whereDate('a.created_at', '>=', $from_date)
-            ->whereDate('a.created_at', '<=', $to_date)
-            ->select($columns)
-            ->orderByDesc('created_at')
-            ->get();
+        $deboning_data = $this->fetchData('deboned_data', $columns, $dates['from_date'], $dates['to_date']);
 
         return response()->json($deboning_data);
     }
