f3d10590d35afc0db92b7a4495c121fac8037d46
Ephantus Karanja <ephantusslogan@gmail.com>
2024-09-30 08:32:42 +0300

    added import Http

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,6 +6,7 @@ use App\Models\Helpers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 
 class ApiController extends Controller
