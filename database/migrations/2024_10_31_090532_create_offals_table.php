10086e66bbc7ec9df3f05b9e3efb707f8ac84fd2
Adiela Abishua <adiela.abishua@gmail.com>
2024-10-31 09:21:25 +0300

    Create offals table

---
+++ database/migrations/2024_10_31_090532_create_offals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offals', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_code', 20)->nullable();
+            $table->foreign('product_code')->references('code')->on('products')->onDelete('set null');
+            $table->double('scale_reading', 8, 2);
+            $table->double('net_weight', 8, 2);
+            $table->boolean('is_manual')->default(0);
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offals');
+    }
+}
