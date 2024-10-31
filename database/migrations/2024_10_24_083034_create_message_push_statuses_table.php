f1671e2fb71f81b473b7128a7f320bee85a766b8
Ephantus Karanja <ephantusslogan@gmail.com>
2024-10-31 08:23:12 +0300

    rename migration class

--- database/migrations/2024_10_24_083034_create_message_push_statuses_table.php
+++ database/migrations/2024_10_24_083034_create_message_push_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMessagePushStatusTable extends Migration
+class CreateMessagePushStatusesTable extends Migration
 {
     /**
      * Run the migrations.
