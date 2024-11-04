6d6c363e8d23513a63e6f3d2ca4e1103e010fbe0
Adiela Abishua <adiela.abishua@gmail.com>
2024-11-04 11:29:00 +0300

    Headers: add search form via inheritance

--- resources/views/layouts/headers/sausage_header.blade.php
+++ resources/views/layouts/headers/sausage_header.blade.php
@@ -64,17 +64,7 @@
             </ul>
 
             <!-- SEARCH FORM -->
-            <form class="form-inline ml-0 ml-md-3">
-                <div class="input-group input-group-sm">
-                    <input class="form-control form-control-navbar" type="search" placeholder="Search"
-                        aria-label="Search">
-                    <div class="input-group-append">
-                        <button class="btn btn-navbar" type="submit">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-            </form>
+            @include('layouts.partials.nav_search_form')
         </div>
 
         <!-- Right navbar links -->
