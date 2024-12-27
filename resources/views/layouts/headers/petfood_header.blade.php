ef1482ee01a4556642558d9996ce9824f91f5e87
Adiela Abishua <adiela.abishua@gmail.com>
2024-12-27 13:07:54 +0300

    Petfood: create header

---
+++ resources/views/layouts/headers/petfood_header.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.headers.template_header')
+
+@section('main-link')
+<a href="{{ route('petfood_dashboard') }}" class="navbar-brand">
+    <img src="{{ asset('assets/img/fcl1.png') }}" alt="FCL Calibra Logo"
+        class="brand-image" style="">
+    <span class="brand-text font-weight-light"><strong> FCL Weight Management System</strong></span>
+</a>
+@endsection
+
+@section('navlist')
+<!-- Left navbar links -->
+<ul class="navbar-nav">
+</ul>
+@endsection
