428c5d2443202e78cc6b8a5c8c9d40de1cdcada4
Adiela Abishua <adiela.abishua@gmail.com>
2024-12-30 10:28:25 +0300

    Petfood Header: update link for issue IDT

--- resources/views/layouts/headers/petfood_header.blade.php
+++ resources/views/layouts/headers/petfood_header.blade.php
@@ -16,22 +16,26 @@
         <a href="{{ route('petfood_dashboard') }}" class="nav-link">Dashboard</a>
     </li>
     <li class="nav-item">
-        <a id="dropdownSubMenu1"  href="{{ route('issue_idt') }}"aria-haspopup="true" aria-expanded="false" class="nav-link">Issue IDT </a>
+        <a id="dropdownSubMenu1"  href="{{  route('list_issued_idt', ['from_location' => '3035', 'to_location' => '3535']) }}"aria-haspopup="true" aria-expanded="false" class="nav-link">Issue IDT </a>
     </li>
     <li class="nav-item dropdown">
         <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
             class="nav-link dropdown-toggle">Receive IDT</a>
         <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-            <li><a href="{{ route('list_receive', ['from_location' => '1570', 'to_location' => '3035']) }}" class="dropdown-item">Butchery -IDTs</a>
+            <li>
+                <a href="{{ route('list_receive', ['from_location' => '1570', 'to_location' => '3035']) }}" class="dropdown-item">Butchery -IDTs</a>
             </li>
-            <hr class="dropdown-divider"/>
-            <li><a href="{{ route('list_receive', ['from_location' => '2595', 'to_location' => '3035']) }}" class="dropdown-item">Highcare -IDTs</a>
+                <hr class="dropdown-divider"/>
+            <li>
+                <a href="{{ route('list_receive', ['from_location' => '2595', 'to_location' => '3035']) }}" class="dropdown-item">Highcare -IDTs</a>
             </li>
-            <hr class="dropdown-divider" />
-            <li><a href="{{ route('list_receive', ['from_location' => '2055', 'to_location' => '3035']) }}" class="dropdown-item">Sausage -IDTs</a>
+                <hr class="dropdown-divider" />
+            <li>
+                <a href="{{ route('list_receive', ['from_location' => '2055', 'to_location' => '3035']) }}" class="dropdown-item">Sausage -IDTs</a>
             </li>
-            <hr class="dropdown-divider" />
-            <li><a href="{{ route('list_receive', ['from_location' => '3535', 'to_location' => '3035']) }}" class="dropdown-item">Despatch -IDTs</a>
+                <hr class="dropdown-divider" />
+            <li>
+                <a href="{{ route('list_receive', ['from_location' => '3535', 'to_location' => '3035']) }}" class="dropdown-item">Despatch -IDTs</a>
             </li>
         </ul>
     </li>
