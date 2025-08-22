cbf91b994aaae6660e3799d6153889c46974bc68
Ephantus Karanja <ephantusslogan@gmail.com>
2025-08-22 12:10:39 +0300

    remove debug text

--- resources/views/despatch/issue-idt.blade.php
+++ resources/views/despatch/issue-idt.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.despatch_master')
 
 @section('content-header')
-<h1 class="m-2">Issue IDTs from Despatch tt</h1>
+<h1 class="m-2">Issue IDTs from Despatch</h1>
 @endsection
 
 @section('content')
