f9d41838493f965b0308acb58250cba5b16a13e9
Ephantus Karanja <ephantusslogan@gmail.com>
2026-02-09 11:57:16 +0300

    remove QA transfer

--- resources/views/highcare1/idt-bulk.blade.php
+++ resources/views/highcare1/idt-bulk.blade.php
@@ -29,7 +29,6 @@
                             <option value="1570">Butchery</option>
                             <option value="2055">Sausage</option>
                             <option value="3535">Despatch</option>
-                            <option value="4450">QA</option>
                         </select>
                     </div>
                     <div class="col-md-6">
