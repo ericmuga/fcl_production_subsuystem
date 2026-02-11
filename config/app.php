2a517dc338c771bf32f7fef1f114477dcd02be1d
Ephantus Karanja <ephantusslogan@gmail.com>
2026-02-11 14:55:24 +0300

    remove debugbar

--- config/app.php
+++ config/app.php
@@ -254,7 +254,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
